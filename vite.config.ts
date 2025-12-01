64df7aa24356b9a0f03b000617d31da3c874414d
ravidulundu <ravidulundu@gmail.com>
2025-12-01 11:49:53 -0300

    chore: remove unused env variable from vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   return {
     server: {
       port: 3000,
